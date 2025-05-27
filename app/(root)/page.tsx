6eb0837fdb9cfc183af8b2af49222af4969ec02e
Bosto <bostoganashvilinika2019@gmail.com>
2025-05-27 23:52:11 +0400

    added hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import '../globals.css'
+
 export default function Home() {
-    return  (
+    return (
         <>
-            <h1 className="text-2xl">Home</h1>
+            <section className="w-full h-[300px] pattern pink_container">
+                <h1 className="heading">Pitch Your Startup</h1>
+                <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+            </section>
         </>
     )
 }
